8d0ca7268641824351b396e9792d7cdb52affe2f
EnzyI <zyxrywsp@gmail.com>
2026-02-02 15:45:04 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,3 @@
-pluginManagement {
-    repositories {
-        mavenCentral()
-        maven("https://repo.papermc.io/repository/maven-public/")
-        gradlePluginPortal()
-    }
-}
-
 rootProject.name = "Swift"
 include("Swift-API")
 include("Swift-Server")
