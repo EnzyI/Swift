c46195a299ab56f2ada06011a4fd6e128a25c1e5
EnzyI <zyxrywsp@gmail.com>
2026-02-02 15:38:21 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,18 @@
 plugins {
     java
-    id("io.papermc.paperweight.userdev") version "1.7.3" apply false
+    id("io.papermc.paperweight.userdev") version "1.7.1" apply false
 }
 
 allprojects {
     group = "com.swift.server"
     version = "1.0.0-SNAPSHOT"
+
     repositories {
         mavenCentral()
         maven("https://repo.papermc.io/repository/maven-public/")
     }
 }
 
-// Chỗ này phải khớp 100% với tên thư mục con
 project(":Swift-Server") {
     apply(plugin = "java")
     apply(plugin = "io.papermc.paperweight.userdev")
@@ -22,7 +22,11 @@ project(":Swift-Server") {
     }
 
     dependencies {
-        // Dùng 'add' để chắc chắn không bị lỗi check kiểu của Kotlin
+        // Quay lại với bản "huyền thoại" ổn định
         add("paperweightDevelopmentBundle", "io.papermc.paper:dev-bundle:1.20.4-R0.1-SNAPSHOT")
     }
+}
+
+project(":Swift-API") {
+    apply(plugin = "java")
 }
