abe6d77d5f6f37dc7033c323f9b90c61e027c482
EnzyI <zyxrywsp@gmail.com>
2026-02-02 16:11:48 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
-    id("io.papermc.paperweight.patcher") version "1.7.1" apply false
+    id("io.papermc.paperweight.userdev") version "1.7.1" apply false
 }
 
 allprojects {
+    group = "com.swift.server"
+    version = "1.0.0-SNAPSHOT"
     repositories {
         mavenCentral()
         maven("https://repo.papermc.io/repository/maven-public/")
