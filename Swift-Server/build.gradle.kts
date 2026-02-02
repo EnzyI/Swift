68220f67720e81288bdea28c0fab1ce0a05f76c1
EnzyI <zyxrywsp@gmail.com>
2026-02-02 15:56:12 +0700

    Update build.gradle.kts

--- Swift-Server/build.gradle.kts
+++ Swift-Server/build.gradle.kts
@@ -1,18 +1,15 @@
 plugins {
     java
-    id("io.papermc.paperweight.userdev") version "1.7.1"
+    // Vẫn dùng userdev nhưng phải đi kèm với cấu hình đặc biệt bên dưới
+    id("io.papermc.paperweight.userdev") version "1.7.1" 
+}
+
+// Đây là "chìa khóa" để mở task applyPatches
+paperweight {
+    serverProject.set(project(":Swift-Server"))
 }
 
 dependencies {
-    // Đây là dòng quan trọng nhất để kích hoạt applyPatches
-    paperweightDevelopmentBundle("io.papermc.paper:dev-bundle:1.20.4-R0.1-SNAPSHOT")
-}
-
-java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
-}
-
-// Thêm dòng này để ép Paperweight nhận diện đúng mode làm việc
-paperweight {
-    // Cấu hình này giúp Plugin biết bạn đang chuẩn bị can thiệp mã nguồn
+    // Phải dùng 'paperweightDevelopmentBundle' thì task mới xuất hiện
+    add("paperweightDevelopmentBundle", "io.papermc.paper:dev-bundle:1.20.4-R0.1-SNAPSHOT")
 }
