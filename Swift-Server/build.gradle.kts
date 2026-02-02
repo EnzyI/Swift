ee53b8962bcc4dbedb72c0a788e59e936237de97
EnzyI <zyxrywsp@gmail.com>
2026-02-02 16:03:01 +0700

    Update build.gradle.kts

--- Swift-Server/build.gradle.kts
+++ Swift-Server/build.gradle.kts
@@ -3,11 +3,13 @@ plugins {
     id("io.papermc.paperweight.userdev") version "1.7.1"
 }
 
-java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
-}
+
 
 dependencies {
-    // Dòng này chính là "ngòi nổ" để hiện ra task applyPatches
+    // Đây là dòng duy nhất kích hoạt việc giải mã Minecraft để làm Fork
     paperweightDevelopmentBundle("io.papermc.paper:dev-bundle:1.20.4-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
